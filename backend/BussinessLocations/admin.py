bd09fb75716437079557849e5a74a1956c4a4376
maros Studenic <studenic.maros@gmail.com>
2023-04-01 21:58:25 +0200

    add MHD to admin

--- backend/BussinessLocations/admin.py
+++ backend/BussinessLocations/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 #
-from .models import POI, Home
+from .models import POI, Home, MHD
 
 admin.site.register(POI)
 admin.site.register(Home)
+admin.site.register(MHD)
+
 
