6cbfe21138ec7393be82f5a5813ba3925bf01f92
Boris Ivanko <borisivanko004@gmail.com>
2023-04-01 19:04:31 +0200

    add react-router-dom package and Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,16 @@
+import Navbar from "./components/Navbar.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Map from './containers/Map.jsx'
+
 function App() {
     return (
-        <h1 className='bg-blue-500'>Test</h1>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<h1>test</h1>}/>
+                <Route path='/map' element={<Map/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ frontend/src/containers/Map.jsx
@@ -0,0 +1,9 @@
+function Map () {
+    return (
+        <>
+            <h1>The Map component</h1>
+        </>
+    )
+}
+
+export default Map
