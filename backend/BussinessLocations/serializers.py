92229723c201e46c2448d8089aa2bf2b7fbe55cb
maros Studenic <studenic.maros@gmail.com>
2023-04-01 21:45:40 +0200

    change json to geojson

--- backend/BussinessLocations/serializers.py
+++ backend/BussinessLocations/serializers.py
@@ -7,6 +7,31 @@ class POISerializer(serializers.HyperlinkedModelSerializer):
         model = POI
         fields = ['name', 'x', 'y', 'typ_0', 'typ_1', 'poly_15' ]
 
+        # {
+        #     type: "FeatureCollection",
+        #     features: [
+        #         {
+        #             type: "Feature",
+        #             geometry: {
+        #                 type: "Point",
+        #                 coordinates: [0, 0]
+        #             },
+        #             properties: {title: "HeatmapPts", id: 111, name: "sample 1"}
+        #         },
+        #     ]
+        # }
+
+    def to_representation(self, instance):
+        print(dir(instance), instance.name)
+        response_dict = dict()
+        response_dict["type"] = "Feature"
+        response_dict["geometry"] = {
+            "type": "Point",
+            "coordinates": [instance.x, instance.y]
+        }
+        response_dict["properties"] = { "title": instance.name}
+        return response_dict
+
 
 class HomeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
