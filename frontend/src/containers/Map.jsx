27f0b05e758b432497d65b236beba8e2885dd4da
Boris Ivanko <borisivanko004@gmail.com>
2023-04-02 10:23:05 +0200

    edit map styling

--- frontend/src/containers/Map.jsx
+++ frontend/src/containers/Map.jsx
@@ -67,7 +67,7 @@ function Map () {
 
                     {showCategories &&
                         <div className="form-group">
-                            <div className="w-full bg-gray-800 flex flex-wrap py-4 px-4" >
+                            <div className="bg-gray-800 hover:bg-gray-900 text-white flex flex-wrap py-2 px-4 mx-2 my-2">
                                 <div className="cursor-pointer" onClick={() => {  setShowTitles(!showTitles) }}>
                                     <p className="text-base font-semibold text-center text-white">
                                         {showTitles ? "Hide Titles": "Show titles"}
