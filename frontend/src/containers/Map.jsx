799ed57eb15e8eb0a0d1212b3445f639647bd0de
Boris Ivanko <borisivanko004@gmail.com>
2023-04-02 04:49:06 +0200

    conditionally show categories

--- frontend/src/containers/Map.jsx
+++ frontend/src/containers/Map.jsx
@@ -7,6 +7,7 @@ function Map () {
     features: []});
     const [categories, setCategories] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('pharmacy');
+    const [showCategories, setShowCategories] = useState(true)
 
     useEffect(() => {
         api.get(`/list-pois?category=${selectedCategory}`)
@@ -40,7 +41,15 @@ function Map () {
         <>
             <div className="form-group">
                 <div className="w-full bg-gray-800 flex flex-wrap py-4 px-4" >
-                    {categories.map((category, index) => {
+                    <div className={`w-full transition flex items-center justify-center text-white ${!showCategories && 'rotate-180'}`} onClick={() => setShowCategories((prevState) => !prevState)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd"
+                                  d="M10 14.586L3.707 8.293a1 1 0 011.414-1.414L10 11.758l4.879-4.879a1 1 0 111.414 1.414L10 14.586z"
+                                  clipRule="evenodd" />
+                        </svg>
+                    </div>
+
+                    {showCategories && categories.map((category) => {
                         return   <div key={category}
                                 className={`px-4 py-2 mx-2 my-2 cursor-pointer ${category === selectedCategory ? "bg-[#32CD32]" : "bg-[#f9eaea]"} `} onClick={() => {  setSelectedCategory(category) }}>
                         <p className="text-base font-semibold text-center text-black">
@@ -48,7 +57,6 @@ function Map () {
                         </p>
                         </div>
                     })}
-
                 </div>
             </div>
 
